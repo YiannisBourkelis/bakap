a4f5fd6ed7440cf087435eb426dc291e99c0d208
Yiannis Bourkelis <yiannis1234@gmail.com>
2025-10-12 01:12:15 +0300

    Refine Samba VFS audit logging to limit success events and set logging facility to local1

--- src/server/create_user.sh
+++ src/server/create_user.sh
@@ -87,7 +87,7 @@ setup_samba_share() {
    # VFS audit module for tracking SMB file operations
    vfs objects = full_audit
    full_audit:prefix = %u|%I|%m
-   full_audit:success = connect disconnect open close write pwrite mkdir rmdir rename unlink
+   full_audit:success = connect disconnect open close write pwrite
    full_audit:failure = none
    full_audit:facility = local1
    full_audit:priority = notice
